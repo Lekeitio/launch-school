90f5e58c1ff39994ef0634779276bbaaa4d65693
Luis <kilinkala@gmail.com>
2016-10-11 18:53:25 +0200

    exercises

---
+++ conditional_loop.rb
@@ -0,0 +1,11 @@
+#conditional_loop.rb
+
+x = 0
+
+while x <= 10
+	if x.even?
+		puts x
+	end
+
+	x = x +1
+end
---
+++ name.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "What is your family name?"
+family_name = gets.chomp
+
+puts "Your full name is: " + name + " " +family_name
+
+10.times do |n|
+	puts name
+end
