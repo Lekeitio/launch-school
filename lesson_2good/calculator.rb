e04e5f670827220c956347f47e6a1e52ced64345
Luis <kilinkala@gmail.com>
2016-10-12 14:45:39 +0200

    lesson 2 v.4

--- lesson_2good/calculator.rb
+++ lesson_2good/calculator.rb
@@ -5,24 +5,24 @@
 # output the result
 
 def prompt(message)
-	Kernel.puts("=> #{message}")
+  Kernel.puts("=> #{message}")
 end
 
 def valid_number?(num)
-	num.to_i() != 0
+  num.to_i().nonzero?
 end
 
 def operation_to_message(op)
-	case op
-	when '1'
-		'Adding'
-	when '2'
-		'Substracting'
-	when '3'
-		'Multiplying'
-	when '4'
-		'Dividing'
-	end
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Substracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
 end
 
 prompt("Welcome to Calculator! Enter your name")
@@ -30,63 +30,61 @@ prompt("Welcome to Calculator! Enter your name")
 name = ''
 
 loop do
-	name = Kernel.gets().chomp()
+  name = Kernel.gets().chomp()
 
-	if name.empty?()
-		prompt("Make sure to use a valid name")
-	else
-		break
-	end
+  if name.empty?()
+    prompt("Make sure to use a valid name")
+  else
+    break
+  end
 end
 
 prompt("Hi #{name}")
 
 loop do
-	number1 = nil
-	number2 = nil
+  number1 = nil
+  number2 = nil
 
-	loop do
-		prompt("What's your first number?")
-		number1 = Kernel.gets().chomp()
+  loop do
+    prompt("What's your first number?")
+    number1 = Kernel.gets().chomp()
 
-		if valid_number?(number1)
-			break
-		else
-			prompt("Hmm... that doesn't look like a valid number")
-		end
-	end
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
 
-	loop do
-		prompt("What's your second number?")
-		number2 = Kernel.gets().chomp()
+  loop do
+    prompt("What's your second number?")
+    number2 = Kernel.gets().chomp()
 
-		if valid_number?(number2)
-			break
-		else
-			prompt("Hmm... that doesn't look like a valid number")
-		end
-	end
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
 
+  # ask the user for operation
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) Add
+    2) Substract
+    3) Multiply
+    4) Divide
+  MSG
 
-
-	# ask the user for operation
-	operator_prompt = <<-MSG
-		What operation would you like to perform?
-		1) Add
-		2) Substract
-		3) Multiply
-		4) Divide
-	MSG
-
-	prompt(operator_prompt)
+  prompt(operator_prompt)
 	
-	operator = ''
-	loop do
-		operator = Kernel.gets().chomp()
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
 
-		if %w(1 2 3 4).include?(operator)
-			break
-		else
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
 			prompt(" 1, 2, 3 or 4")
 		end
 	end
@@ -94,23 +92,21 @@ loop do
 	prompt("#{operation_to_message(operator)} the two numbers...")
 
 	result = case operator
-		when '1'
-			number1.to_i() + number2.to_i()
-		when '2'
-			number1.to_i() - number2.to_i()
-		when '3'
-			number1.to_i() * number2.to_i()
-		when '4'
-			number1.to_f() / number2.to_f()
-	end
+							when '1'
+								number1.to_i() + number2.to_i()
+							when '2'
+								number1.to_i() - number2.to_i()
+							when '3'
+								number1.to_i() * number2.to_i()
+							when '4'
+								number1.to_f() / number2.to_f()
+					 end
 
-	prompt("The result for #{operation_to_message(operator).downcase()} is #{result}")
+  prompt("The result for #{operation_to_message(operator).downcase()} is #{result}")
 
-	prompt("Do you want to do another calculation?  (Y/N)")
-	answer = Kernel.gets().chomp()
-
-	break unless answer.downcase().start_with?('y')
-
+  prompt("Do you want to do another calculation?  (Y/N)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end
 
 prompt("Thank you for using the Calculator")
